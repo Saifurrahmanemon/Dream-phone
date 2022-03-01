6a3b157efbce8c95a8549a398203bf5882e17799
Saifurrahmanemon <saifemon789@gmail.com>
2022-03-01 15:56:21 +0600

    finish with mobile details

--- js/app.js
+++ js/app.js
@@ -5,30 +5,75 @@ const getMobileResult = async () => {
     const res = await fetch(url);
     const data = await res.json();
     showMobileResult(data.data);
+    inputResult.value = "";
 };
 
 // for showing mobile results
 const showMobileResult = (mobiles) => {
     const showAllMobiles = document.getElementById("show-all-mobiles");
+    // for resetting result
+    showAllMobiles.textContent = "";
     mobiles.map((mobile) => {
-        console.log(mobile.slug);
+        // destructuring elements from mobile
+        const { phone_name, brand, slug, image } = mobile;
         const showSingleMobile = document.createElement("div");
         showSingleMobile.classList.add("col");
         showSingleMobile.innerHTML = `
-             <div  class="card ">
-                    <img src="${mobile.image}" class="card-img-top w-75 mx-auto"  alt="..." />
+             <div  class="card shadow p-3 mb-5 bg-white rounded">
+                    <img src="${image}" class="card-img-top w-75 mx-auto"  alt="..." />
                     <div class="card-body">
-                        <h5 class="card-title">${mobile.phone_name}</h5>
-                        <h6 class="card-title mb-3">${mobile.brand}</h6>
-                        <button onclick='showMobileDetails(${mobile.slug})'  class="btn btn-outline-primary">Show details</button>
+                        <h5 class="card-title">${phone_name}</h5>
+                        <h6 class="card-title mb-3">${brand}</h6>
+                        <button onclick="getMobileDetails('${slug}')"  class="btn btn-outline-primary">Show details</button>
                     </div>
                 </div>
         `;
         showAllMobiles.appendChild(showSingleMobile);
     });
 };
+// receiving mobile details api
+const getMobileDetails = async (mobileId) => {
+    url = ` https://openapi.programming-hero.com/api/phone/${mobileId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    showMobileDetails(data.data);
+};
 
-const showMobileDetails = (mobileId) => {
-    url = `https://openapi.programming-hero.com/api/phone/${mobileId}`;
-    console.log(url);
+// for showing specific mobile details
+const showMobileDetails = (details) => {
+    const getMobileDetailsDiv = document.getElementById("mobile-details");
+    const showMobileDetail = document.createElement("div");
+    const {
+        name,
+        image,
+        brand,
+        releaseDate,
+        mainFeatures: { storage, displaySize, chipSet, memory, sensors },
+    } = details;
+    showMobileDetail.classList.add(
+        "card",
+        "my-3",
+        "shadow",
+        "mb-5",
+        "bg-white",
+        "rounded"
+    );
+
+    showMobileDetail.innerHTML = `
+    <img class="card-img-top w-50 mx-auto pt-2" src="${image}" alt="Card image cap">
+     <div class="card-body">
+       <h3 class="card-title text-center">${name}</h3>
+       <h6 class="card-title text-center">${
+           releaseDate ? releaseDate : "No release date found"
+       }</h6>
+       <h4 class="my-3">Main Features</h4>
+        <div class ="text-feature">
+        <p><span>Storage:</span> ${storage}</p>
+        <p> <span>Display Size:</span> ${displaySize}</p>
+        <p> <span>Chip Set:</span> ${chipSet}</p>
+        <p> <span>Memory:</span> ${memory}</p>
+        </div>
+     </div>
+    `;
+    getMobileDetailsDiv.appendChild(showMobileDetail);
 };
